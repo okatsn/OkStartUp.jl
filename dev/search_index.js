d43d1d5487bb4d71d3a7e3f3f3aaced52e9f1266
Documenter.jl <documenter@juliadocs.github.io>
2023-12-26 03:29:22 +0000

    build based on 47949fb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkStartUp","category":"page"},{"location":"#OkStartUp","page":"Home","title":"OkStartUp","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkStartUp.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkStartUp]","category":"page"}]
+}
